448761e6ada4370fb62f2b04ee1f3a9f7537326c
chigvintseva <al.chigvintseva@gmail.com>
2025-11-17 01:00:36 +0100

    main setup, structs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:57:15 by achigvin          #+#    #+#             */
-/*   Updated: 2025/11/16 22:50:38 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/11/17 00:07:07 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,11 @@ int	main(void)
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, W_WIDTH, W_HEIGHT, "My attempt");
 	
-	//new img
-	img.img = mlx_new_image(mlx, 800, 600);
-	img.addr = mlx_get_data_addr(img.img, 
-		&img.bits_per_pixel, 
-		&img.line_length, 
-		&img.endian);
-	
-	//draw dots in colours
-	my_mlx_pixel_put(&img, 10, 10, 0x00FF0000);
-	my_mlx_pixel_put(&img, 20, 20, 0x000000FF);
-	my_mlx_pixel_put(&img, 30, 30, 0x0000FF00);
-	
-	// show the image inside the windows
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 		
-	// loop till ctrl c
+	// GAME LOOP
 	mlx_loop(mlx);
+
+	
 	//cleaning up
 	mlx_destroy_window(mlx, mlx_win);
 	mlx_destroy_display(mlx);
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:22:03 by achigvin          #+#    #+#             */
-/*   Updated: 2025/11/16 22:47:02 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/11/17 00:58:12 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,46 @@
 # include <math.h>
 # include "sys/time.h"
 
-# define W_WIDTH 800
-# define W_HEIGHT 600
+# define W_WIDTH 80
+# define W_HEIGHT 80
 
-// couolrs defines in hex:
+// player position
+typedef struct s_position
+{
+	unsigned int	x;
+	unsigned int	y;
+}	t_position;
 
-typedef struct s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}	t_data;
+// to store the images (textures folder)
+typedef struct s_image
+{
+	void				*img;
+	char				*address;
+	unsigned int		width;
+	unsigned int		height;
+}	t_image;
+
+// map info
+typedef struct s_map
+{
+	char			**map_sketch;
+	unsigned int	rows;
+	unsigned int	columns;
+}	t_map;
+
+// all game info (prev.structs) + playing setup n moves
+typedef struct s_game
+{
+	void			*mlx;
+	void			*mlx_win;
+	unsigned int	moves;
+	t_map			map;
+	t_position		player;
+	t_image			wall;
+	t_image			collect;
+	t_image			exit;
+	t_image			space;
+}	t_game;
 
 
 #endif
