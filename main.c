233485f3faf58c9b3b37e9a0644e026a4ca24e20
chigvintseva <al.chigvintseva@gmail.com>
2025-11-17 03:41:44 +0100

    error free functions r done for all cases - error early, arror and succes after map already exists. user input argv checker is done

--- main.c
+++ main.c
@@ -6,66 +6,58 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:57:15 by achigvin          #+#    #+#             */
-/*   Updated: 2025/11/17 03:24:53 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/11/17 03:40:29 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	error_exit(char *message)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(message, 2);
+	exit(EXIT_FAILURE);
+}
 
-void	error_exit(t_game *game, char *message)
+void	error_exit_game(t_game *game, char *message)
 {
 	int	i;
-	
-	if (game)
+
+	if (game->map.map_sketch)
 	{
-		if (game->map.map_sketch)
+		i = 0;
+		while (i < game->map.rows)
 		{
-			i = 0;
-			while (i < game->map.rows)
-			{
-				free(game->map.map_sketch[i]);
-				i++;
-			}
-			free(game->map.map_sketch);
+			free(game->map.map_sketch[i]);
+			i++;
 		}
+		free(game->map.map_sketch);
 	}
 	if (game->mlx_win)
 		mlx_destroy_window(game->mlx, game->mlx_win);
 	if (game->mlx)
-	{
 		mlx_destroy_display(game->mlx);
-		free(game->mlx);
-	}
-	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd(message, 2);
-	exit(EXIT_FAILURE);
+	error_exit(message);
 }
 
 void	success_game(t_game *game)
 {
 	int	i;
 
-	if (game)
+	if (game->map.map_sketch)
 	{
-		if (game->map.map_sketch)
+		i = 0;
+		while (i < game->map.rows)
 		{
-			i = 0;
-			while (i < game->map.rows)
-			{
-				free(game->map.map_sketch[i]);
-				i++;
-			}
-			free(game->map.map_sketch);
+			free(game->map.map_sketch[i]);
+			i++;
 		}
+		free(game->map.map_sketch);
 	}
 	if (game->mlx_win)
 		mlx_destroy_window(game->mlx, game->mlx_win);
 	if (game->mlx)
-	{
 		mlx_destroy_display(game->mlx);
-		free(game->mlx);
-	}
 	ft_putstr_fd("Success! You finished the game!\n", 1);
 	exit(EXIT_SUCCESS);
 }
@@ -74,7 +66,8 @@ int	main(int argc, char **argv)
 {
 	t_game	game;
 
-	check_input(argc, argv, &game);
+	check_input(argc, argv);
+	init_game(&game);
 	// load_map(&game, argv[1]);
 	// if (map_valid(&game) == 0)
 	// 	invalid_map_error(&game);
@@ -83,6 +76,6 @@ int	main(int argc, char **argv)
 	// render_map(&game);
 	// mlx_key_hook(game.mlx_win, key_handler(&game), &game);
 	// GAME LOOP
-	mlx_loop(game.mlx);
+	//mlx_loop(game.mlx);
 	return (0);
 }
---
+++ user_input.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   user_input.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 03:03:11 by achigvin          #+#    #+#             */
+/*   Updated: 2025/11/17 03:38:29 by achigvin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	check_input(int argc, char **argv)
+{
+	size_t	len;
+	int		fd;
+
+	if (argc != 2)
+		error_exit("Invalid input.\nValid example: ./so_long maps/map.ber\n");
+	len = ft_strlen(argv[1]);
+	if (len < 5)
+		error_exit("Invalid filename\n");
+	if (ft_strncmp(&argv[1][len - 4], ".ber", 4) != 0)
+		error_exit("Invalid filename or filetype.\n");
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error\n Not able to open map file\n");
+		exit(EXIT_FAILURE);
+	}
+	close(fd);	
+}
+
+void	init_game(t_game *game)
+{
+	game->mlx = NULL;
+	game->mlx_win = NULL;
+	game->moves = 0;
+	game->map.map_sketch = NULL;
+	game->map.rows = 0;
+	game->map.columns = 0;
+	game->player.x = 0;
+	game->player.y = 0;
+}
