a41a11bf6589f812d59773d260e7e419f6cc805b
chigvintseva <al.chigvintseva@gmail.com>
2025-11-17 03:25:14 +0100

    main with error and succes closing:

--- main.c
+++ main.c
@@ -6,44 +6,83 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:57:15 by achigvin          #+#    #+#             */
-/*   Updated: 2025/11/17 00:07:07 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/11/17 03:24:53 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-// void	exit_with_error()
-// {
-// 	write(1, "Error\n", 6);
-// 	exit_error();
-// }
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int colour)
+void	error_exit(t_game *game, char *message)
 {
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length
-		+ x * (data->bits_per_pixel / 8));
-	*(unsigned int *)dst = colour;
+	int	i;
+	
+	if (game)
+	{
+		if (game->map.map_sketch)
+		{
+			i = 0;
+			while (i < game->map.rows)
+			{
+				free(game->map.map_sketch[i]);
+				i++;
+			}
+			free(game->map.map_sketch);
+		}
+	}
+	if (game->mlx_win)
+		mlx_destroy_window(game->mlx, game->mlx_win);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(message, 2);
+	exit(EXIT_FAILURE);
 }
 
-int	main(void)
+void	success_game(t_game *game)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	int	i;
 
-	//windows creation
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, W_WIDTH, W_HEIGHT, "My attempt");
-	
-		
+	if (game)
+	{
+		if (game->map.map_sketch)
+		{
+			i = 0;
+			while (i < game->map.rows)
+			{
+				free(game->map.map_sketch[i]);
+				i++;
+			}
+			free(game->map.map_sketch);
+		}
+	}
+	if (game->mlx_win)
+		mlx_destroy_window(game->mlx, game->mlx_win);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
+	ft_putstr_fd("Success! You finished the game!\n", 1);
+	exit(EXIT_SUCCESS);
+}
+
+int	main(int argc, char **argv)
+{
+	t_game	game;
+
+	check_input(argc, argv, &game);
+	// load_map(&game, argv[1]);
+	// if (map_valid(&game) == 0)
+	// 	invalid_map_error(&game);
+	// start_game(&game, argv[1]);
+	// load_textures(&game);
+	// render_map(&game);
+	// mlx_key_hook(game.mlx_win, key_handler(&game), &game);
 	// GAME LOOP
-	mlx_loop(mlx);
-
-	
-	//cleaning up
-	mlx_destroy_window(mlx, mlx_win);
-	mlx_destroy_display(mlx);
+	mlx_loop(game.mlx);
 	return (0);
 }
